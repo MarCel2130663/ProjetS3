4320041d60192ab29bd3d006322171103174d877
Celeste Martel <cely.martel@hotmail.ca>
2025-06-12 15:31:10 -0400

    test potentiometre

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 #include <LibS3GRO.h>
 
-int MAGPIN = 2;
+int MAGPIN = 2; //pin electroaimant
+int POTPIN = 0; // pin potentiometre
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(MAGPIN, OUTPUT);
+  pinMode(POTPIN, INPUT);
 }
 
 void loop() {
-  digitalWrite(MAGPIN, HIGH);
+  //electroaimant
+  /*digitalWrite(MAGPIN, HIGH);
   delay(10000);
   digitalWrite(MAGPIN, LOW);
-  delay(1000);
+  delay(1000);*/
+
+  //potentiometre
+  int valeur = analogRead(POTPIN);
+  Serial.println(valeur);
 }
